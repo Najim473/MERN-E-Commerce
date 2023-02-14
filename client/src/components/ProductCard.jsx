ae255ddb04d66b5c76c284e65f07e7cfdb356999
Najim Uddin <najim7304@gmail.com>
2023-02-14 20:26:35 +0600

    created product card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,4 +1,59 @@
-const ProductCard = () => {
-  return <div></div>;
+import {
+  Flex,
+  Circle,
+  Box,
+  Image,
+  Badge,
+  useColorModeValue,
+  Icon,
+  Button,
+  Tooltips,
+  Stack,
+  Link,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
+import { FiShoppingCart } from "react-icons/fi";
+import { Link as ReactLink } from "react-router-dom";
+import { StarIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+const ProductCard = ({ product }) => {
+  console.log(product.image);
+  return (
+    <div>
+      <Stack
+        p={2}
+        spacing="30px"
+        bg={useColorModeValue("white", "gray.800")}
+        minW="240px"
+        h="450px"
+        borderWidth="10px"
+        rounded="lg"
+        shadow="lg"
+        position="relative"
+      >
+        {product.isNew && (
+          <Circle
+            size="10px"
+            position="absolute"
+            top={2}
+            right={2}
+            bg="green.300"
+          />
+        )}
+        {product.stock <= 0 && (
+          <Circle
+            size="10px"
+            position="absolute"
+            top={2}
+            right={2}
+            bg="red.300"
+          />
+        )}
+        <Image scr={product.image} alt={product.name} roundTop="lg" />
+        <Box></Box>
+      </Stack>
+    </div>
+  );
 };
 export default ProductCard;
--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,4 +1,5 @@
 import { Center, Wrap, WrapItem } from "@chakra-ui/react";
+import ProductCard from "../components/ProductCard";
 import { products } from "../products";
 const ProductsScreen = () => {
   return (
@@ -6,7 +7,7 @@ const ProductsScreen = () => {
       {products.map((product) => (
         <WrapItem key={product.id}>
           <Center w="250px" h="550px">
-            {product.name}
+            <ProductCard product={product} />
           </Center>
         </WrapItem>
       ))}
