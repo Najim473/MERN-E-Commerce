330d930885891829c04f0b32169bf9bd2a15b8eb
Najim Uddin <najim7304@gmail.com>
2023-02-15 17:36:12 +0600

    server error solved

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
-import connectToDatabase from "./database";
-import express from 'express'
-
+import dotenv from 'dotenv'
+import connectToDatabase from "./database.js";
+import express from "express";
 dotenv.config();
 connectToDatabase();
 const app = express();
+app.use(express.json());
+
+
+
+
